7bce21baec085934440c8de4a33922c201c7ac2f
lihua <2318737510@qq.com>
2024-04-23 16:50:18 +0800

    master test4 commit

--- src/main/java/com/lihua/git/GitTest.java
+++ src/main/java/com/lihua/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello git3333");
         System.out.println("hello git-master4444");
         System.out.println("hello git-hotfix4444");
+        System.out.println("hello git-hotfix");
 
 
     }
