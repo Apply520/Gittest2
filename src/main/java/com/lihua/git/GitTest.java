867c91c46fd30a809ba3abce2efe39ca0a74a550
Apply520 <161837404+Apply520@users.noreply.github.com>
2024-04-23 17:06:39 +0800

    pull commit

--- src/main/java/com/lihua/git/GitTest.java
+++ src/main/java/com/lihua/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git-hotfix4444");
         System.out.println("hello git-hotfix");
         System.out.println("push test");
+        System.out.println("pull test");
 
 
     }
